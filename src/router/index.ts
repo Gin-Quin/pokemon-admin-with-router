3576f0a2a5424f2848b0b76de0be9d66f2e27d28
lepzulnag <gindre.matthieu@gmail.com>
2021-12-09 10:27:37 +0100

    ✨ add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,21 +18,28 @@ const router = createRouter({
       component: () => import("../layouts/PokemonLayout.vue"),
       children: [
         {
-          path: "/create",
+          path: "create",
           name: "pokemon-new",
           component: () => import("../pages/pokemon/NewPokemon.vue"),
         },
         {
-          path: "/edit/:id",
+          path: "edit/:id",
           name: "pokemon-edit",
           component: () => import("../pages/pokemon/EditPokemon.vue"),
         },
         {
-          path: "/view/:id",
+          path: "view/:id",
           name: "pokemon",
           component: () => import("../pages/pokemon/ViewPokemon.vue"),
         },
       ],
+    },
+
+    // si le path ne matche aucune route, on affiche le component NotFound
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: () => import("../pages/NotFound.vue"),
     },
   ],
 });
